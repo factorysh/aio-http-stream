565024458ec30517a55bfbcf7db1a77212e53b82
Mathieu Lecarme <mlecarme@bearstech.com>
2020-01-21 18:07:40 +0100

    Black.

--- setup.py
+++ setup.py
@@ -1,24 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='aiohttp-stream',
-    version='0.1',
-    description='',
-    url='https://github.com/factorysh/aiohttp-stream',
-    install_requires=[
-        'aiohttp',
-    ],
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    extras_require={
-        'test': ['pytest', 'pytest-cov', 'pytest-mock', 'pytest-asyncio'],
-    },
+    name="aiohttp-stream",
+    version="0.1",
+    description="",
+    url="https://github.com/factorysh/aiohttp-stream",
+    install_requires=["aiohttp",],
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    extras_require={"test": ["pytest", "pytest-cov", "pytest-mock", "pytest-asyncio"],},
     license="3 terms BSD",
     classifiers=[
-      'Operating System :: MacOS',
-      'Operating System :: POSIX',
-      'License :: OSI Approved :: BSD License',
-      'Programming Language :: Python :: 3.5',
-      'Programming Language :: Python :: 3.6',
-      'Programming Language :: Python :: 3.7',
-    ]
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
